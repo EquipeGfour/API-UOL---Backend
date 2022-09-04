47f6c2eced5957df410f89264a1db98277e56f61
Nicolas Lima <nichollaslimma734@gmail.com>
2022-09-04 20:02:45 -0300

    iniciado repositorio da aplicação

---
+++ servico/src/main/java/com/uol/servico/ServicoApplication.java
@@ -0,0 +1,13 @@
+package com.uol.servico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoApplication.class, args);
+	}
+
+}
---
+++ servico/src/test/java/com/uol/servico/ServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uol.servico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
