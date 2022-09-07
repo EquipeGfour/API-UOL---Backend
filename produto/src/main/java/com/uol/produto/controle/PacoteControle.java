293a9156207e4bfa9aeb276cb0de2d92b874a05b
Natalia <natalirabessa@gmail.com>
2022-09-07 20:46:03 -0300

    criado endpoint para criação de pacotes

--- produto/src/main/java/com/uol/produto/controle/PacoteControle.java
+++ produto/src/main/java/com/uol/produto/controle/PacoteControle.java
@@ -1,15 +1,22 @@
 package com.uol.produto.controle;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.uol.produto.entidade.Pacote;
+import com.uol.produto.entidade.Produto;
 import com.uol.produto.repositorio.PacoteRepositorio;
+import com.uol.produto.repositorio.ProdutoRepositorio;
 
 @RestController
 @RequestMapping("/pacote")
@@ -17,6 +24,9 @@ public class PacoteControle {
 
 	@Autowired
 	private PacoteRepositorio repositorio;
+	
+	@Autowired
+	private ProdutoRepositorio repositorioProduto;
 	
 	@GetMapping("/buscar")
 	public List<Pacote> buscarPacotes(){
@@ -28,7 +38,25 @@ public class PacoteControle {
 		return repositorio.findById(id).orElse(null);
 	}
 	
+	@PostMapping("/cadastrar")
+	public String cadastrarPacote (@RequestBody @Valid Pacote pacote) {
+		
+		List<Produto> produtos = new ArrayList<Produto>();
+		
+		for (Produto p:pacote.getProdutos()) {
+			Produto selecionado = repositorioProduto.findById(p.getId()).orElse(null);
+			if(selecionado != null) {
+				produtos.add(selecionado);
+			}
+		}
+		pacote.setProdutos(produtos);
+		Pacote pacoteCriado = repositorio.save(pacote);
+		
+				
+		return pacoteCriado.getId();
+	}
 	
+
 
 
 }
