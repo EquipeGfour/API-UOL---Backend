b86322798df855e43feb811ab334fd76eb4f8718
Rafael Waltrick <rafael.waltrick@fatec.sp.gov.br>
2022-10-26 11:57:20 -0300

    Atualização repositório

--- crud/src/main/java/com/uol/crud/repositorio/OfertaRepositorio.java
+++ crud/src/main/java/com/uol/crud/repositorio/OfertaRepositorio.java
@@ -2,6 +2,8 @@ package com.uol.crud.repositorio;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import com.uol.crud.entidade.Oferta;
+
 public interface OfertaRepositorio extends MongoRepository<Oferta, String>{
 	
 
