39047d6030c0baa063c963e0a7467a53edcfd059
Nicolas Lima <nichollaslimma734@gmail.com>
2022-10-27 10:09:36 -0300

    Criado rota de busca de ofertas

--- crud/src/main/java/com/uol/crud/controle/OfertaControle.java
+++ crud/src/main/java/com/uol/crud/controle/OfertaControle.java
@@ -1,13 +1,17 @@
 package com.uol.crud.controle;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.uol.crud.entidade.Oferta;
 import com.uol.crud.repositorio.OfertaRepositorio;
 
 @RestController
@@ -17,7 +21,10 @@ public class OfertaControle {
 	@Autowired
 	private OfertaRepositorio repositorio;
 	
-	//@GetMapping("/buscar")
+	@GetMapping("/buscar")
+	public List<Oferta> buscarOfertas(){
+	    return repositorio.findAll();
+	}
 	
 	//@GetMapping("/busca/{id}")
 	
